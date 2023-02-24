ffa2448fd800572c89c9002efbefd2c73543d7c2
guilhermedkdk <guilhermeperesmm@gmail.com>
2023-02-24 10:32:01 -0300

    updating files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import "./style.css";
+import data from "./data";
 
-function App() {
+const cards = data.map((item) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Card
+      key={item.id}
+      item={item}
+    />
+  );
+});
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
-
-export default App;
